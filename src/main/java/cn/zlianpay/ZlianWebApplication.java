a387c669bb04279ad5f0060e60684bff2ff1062a
Flour_Mo <1283468667@qq.com>
2021-08-24 16:36:02 +0800

    Run using jar package

--- src/main/java/cn/zlianpay/ZlianWebApplication.java
+++ src/main/java/cn/zlianpay/ZlianWebApplication.java
@@ -3,8 +3,8 @@ package cn.zlianpay;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+/*import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;*/
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.wf.jwtp.configuration.EnableJwtPermission;
 
@@ -12,15 +12,15 @@ import org.wf.jwtp.configuration.EnableJwtPermission;
 @EnableAsync
 @MapperScan("cn.zlianpay.**.mapper")
 @SpringBootApplication
-public class ZlianWebApplication extends SpringBootServletInitializer {
+public class ZlianWebApplication /*extends SpringBootServletInitializer*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(ZlianWebApplication.class, args);
     }
 
-    @Override
+    /*@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ZlianWebApplication.class);
-    }
+    }*/
 
 }
