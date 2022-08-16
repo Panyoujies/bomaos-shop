ae63f106370fe9e811cdec2afb86a1d0a243adec
Flour_Mo <1724962375@qq.com>
2022-08-16 15:42:45 +0800

    Flour_Mo: fix unit test environment

--- src/test/java/cn/zlianpay/ZlianWebApplicationTests.java
+++ src/test/java/cn/zlianpay/ZlianWebApplicationTests.java
@@ -1,8 +1,7 @@
 package cn.zlianpay;
 
-import cn.zlianpay.reception.util.SynchronizedByKey;
 import cn.zlianpay.reception.util.SynchronizedByKeyService;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
